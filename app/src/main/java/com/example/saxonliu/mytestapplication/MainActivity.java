adc2e600dd2750de6a814727c4b9bb836b446307
saxonliu <saxonliu@tencent.com>
2018-10-14 16:57:27 +0800

    init commond

---
+++ app/src/main/java/com/example/saxonliu/mytestapplication/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.saxonliu.mytestapplication;
+
+import android.support.v4.app.SupportActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends SupportActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
